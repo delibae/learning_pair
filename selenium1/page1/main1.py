ed5b0b21ecbca4c8b7f926694cef0f5514a50035
delibae <82585834+delibae@users.noreply.github.com>
2022-10-31 00:26:18 +0900

    selenium4 immigration complete

--- selenium1/page1/main1.py
+++ selenium1/page1/main1.py
@@ -5,83 +5,87 @@ import pandas as pd
 import numpy as np     
 
 from bs4 import BeautifulSoup    
-from selenium import webdriver  
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 import time                      
 import math
 
-import pandas as pd
-
 import csv 
 
-from difflib import SequenceMatcher
 
-path_driver = "chromedriver.exe"
+path_driver = "./chromedriver"
 
-driver = webdriver.Chrome(path_driver)
+chrome_options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+ad_list = ['경기 이천시 아리역로 37','경기 이천시 아리역로38번길 10-18']
+time_dict = {}
 
-ad_list = [['경기 이천시 아리역로 37','경기 이천시 아리역로38번길 10-18']]
-time_list = []
+for i in range(len(ad_list)):
+    for j in range(i+1, len(ad_list)):
+        ad1_name = ad_list[i]
+        ad2_name = ad_list[j]
+        driver.get("https://map.kakao.com/")
 
-for ad1_name, ad2_name in ad_list:
+        time.sleep(3)
 
-    driver.get("https://map.kakao.com/")
+        ad1 = driver.find_element(By.XPATH, '//*[@id="search.keyword.query"]')
+        ad1.send_keys(ad1_name + Keys.ENTER)
+        time.sleep(2)
 
-    time.sleep(3)
+        try:
+            get_out = driver.find_element(By.XPATH,'/html/body/div[10]').click()
+            get_out2 = driver.find_element(By.XPATH,'/html/body/div[10]/div').click()
 
+            time.sleep(1)
+        except:
+            pass
 
-    ad1 = driver.find_element_by_xpath('//*[@id="search.keyword.query"]')
-    ad1.send_keys(ad1_name + Keys.ENTER)
-    time.sleep(2)
-
-    try:
-        get_out = driver.find_element_by_xpath('/html/body/div[10]').click()
-        get_out2 = driver.find_element_by_xpath('/html/body/div[10]/div').click()
+        bt = driver.find_element(By.XPATH,'//*[@id="view.mapContainer"]/div[2]/div/div[6]/div[7]/div/div[2]/div[3]/div/div[2]/button')
+        bt.click()
 
         time.sleep(1)
-    except:
-        pass
-
-    bt = driver.find_element_by_xpath('//*[@id="view.mapContainer"]/div[2]/div/div[6]/div[7]/div/div[2]/div[3]/div/div[2]/button')
-    bt.click()
-
-    time.sleep(1)
 
 
-    start_p = driver.find_element_by_xpath("//button[.='" + '출발' + "']")
-    start_p.click()
+        start_p = driver.find_element(By.XPATH,"//button[.='" + '출발' + "']")
+        start_p.click()
 
-    time.sleep(1)
+        time.sleep(1)
 
-    get_out3 = driver.find_element_by_xpath('/html/body/div[10]/div/div').click()
-    time.sleep(1)
-
-
-    try:
-        muji = driver.find_element_by_xpath('//*[@id="info.route.searchBox.list"]/div[2]').click()
-    except:
-        pass
-
-    ad2 = driver.find_element_by_xpath('//*[@id="info.route.waypointSuggest.input1"]')
-
-    ad2.send_keys(ad2_name + Keys.ENTER)
-
-    time.sleep(2)
+        get_out3 = driver.find_element(By.XPATH,'/html/body/div[10]/div/div').click()
+        time.sleep(1)
 
 
-    car_bt = driver.find_element_by_xpath('//*[@id="cartab"]')
-    car_bt.click()
+        try:
+            muji = driver.find_element(By.XPATH,'//*[@id="info.route.searchBox.list"]/div[2]').click()
+        except:
+            pass
 
-    time.sleep(2)
+        ad2 = driver.find_element(By.XPATH,'//*[@id="info.route.waypointSuggest.input1"]')
 
-    take_time = driver.find_element_by_xpath('//*[@id="info.flagsearch"]/div[6]/ul/li/div[1]/div/div[1]/p/span[1]')
-    text = take_time.text
+        ad2.send_keys(ad2_name + Keys.ENTER)
 
-    time_list.append(text)
+        time.sleep(2)
 
 
-print(time_list)
+        car_bt = driver.find_element(By.XPATH,'//*[@id="cartab"]')
+        car_bt.click()
 
+        time.sleep(2)
+        try:
+            take_time = driver.find_element(By.XPATH,'//*[@id="info.flagsearch"]/div[6]/ul/li/div[1]/div/div[1]/p/span[1]')
+            text = take_time.text
+        except:
+            # print("error: no time value")
+            pass
+        time_dict[f'{i}_{j}'] = text
+
+
+print(1)
+print(time_dict)
 
 driver.close()
