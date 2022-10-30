03798fe2d02719962d359a7d5ee1b3e2252007c2
delibae <82585834+delibae@users.noreply.github.com>
2022-10-31 00:54:16 +0900

    에러 핸들링 및 데이터 피클화 완료

--- selenium1/page1/main1.py
+++ selenium1/page1/main1.py
@@ -15,7 +15,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time                      
 import math
 
-import csv 
+import csv
+
+import pickle
 
 
 path_driver = "./chromedriver"
@@ -23,20 +25,28 @@ path_driver = "./chromedriver"
 chrome_options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-ad_list = ['경기 이천시 아리역로 37','경기 이천시 아리역로38번길 10-18']
+
+ad_list = ['경기 이천시 아리역로 37','경기 이천시 아리역로38번길 10-18','경기 이천시 부발읍 경충대로 1918-18','경기 이천시 설성면 설가로 219','경기 이천시 장호원읍 대서리 산 63-1']
 time_dict = {}
 
 for i in range(len(ad_list)):
     for j in range(i+1, len(ad_list)):
+
         ad1_name = ad_list[i]
         ad2_name = ad_list[j]
+
+        print(ad1_name,ad2_name)
+
         driver.get("https://map.kakao.com/")
 
         time.sleep(3)
+        try:
+            ad1 = driver.find_element(By.XPATH, '//*[@id="search.keyword.query"]')
+            ad1.send_keys(ad1_name + Keys.ENTER)
+            time.sleep(2)
+        except:
+            print("error: first address input error")
 
-        ad1 = driver.find_element(By.XPATH, '//*[@id="search.keyword.query"]')
-        ad1.send_keys(ad1_name + Keys.ENTER)
-        time.sleep(2)
 
         try:
             get_out = driver.find_element(By.XPATH,'/html/body/div[10]').click()
@@ -46,8 +56,9 @@ for i in range(len(ad_list)):
         except:
             pass
 
-        bt = driver.find_element(By.XPATH,'//*[@id="view.mapContainer"]/div[2]/div/div[6]/div[7]/div/div[2]/div[3]/div/div[2]/button')
+        bt = driver.find_element(By.CLASS_NAME,'btn_direction')
         bt.click()
+
 
         time.sleep(1)
 
@@ -57,8 +68,11 @@ for i in range(len(ad_list)):
 
         time.sleep(1)
 
-        get_out3 = driver.find_element(By.XPATH,'/html/body/div[10]/div/div').click()
-        time.sleep(1)
+        try:
+            get_out3 = driver.find_element(By.XPATH,'/html/body/div[10]/div/div').click()
+            time.sleep(1)
+        except:
+            pass
 
 
         try:
@@ -66,26 +80,35 @@ for i in range(len(ad_list)):
         except:
             pass
 
-        ad2 = driver.find_element(By.XPATH,'//*[@id="info.route.waypointSuggest.input1"]')
-
-        ad2.send_keys(ad2_name + Keys.ENTER)
-
-        time.sleep(2)
-
-
-        car_bt = driver.find_element(By.XPATH,'//*[@id="cartab"]')
-        car_bt.click()
+        try:
+            ad2 = driver.find_element(By.XPATH,'//*[@id="info.route.waypointSuggest.input1"]')
+            ad2.send_keys(ad2_name + Keys.ENTER)
+        except:
+            print("error: second address input error")
 
         time.sleep(2)
+
+        try:
+            car_bt = driver.find_element(By.XPATH,'//*[@id="cartab"]')
+            car_bt.click()
+        except:
+            print("error: car tab switch error")
+
+        time.sleep(1)
+
         try:
             take_time = driver.find_element(By.XPATH,'//*[@id="info.flagsearch"]/div[6]/ul/li/div[1]/div/div[1]/p/span[1]')
             text = take_time.text
         except:
-            # print("error: no time value")
-            pass
-        time_dict[f'{i}_{j}'] = text
+            print("error: no time value")
 
+        time_dict[f'{i}_{j}'] = text
 
 print(time_dict)
 
 driver.close()
+
+with open('time_dict.pickle','wb') as fw:
+    pickle.dump(time_dict, fw)
+
+
