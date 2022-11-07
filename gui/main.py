988af77acb107e7a9561fa36f557e26dd96ab315
delibae <82585834+delibae@users.noreply.github.com>
2022-11-07 14:15:38 +0900

    page1~page3 css complete

--- gui/main.py
+++ gui/main.py
@@ -7,13 +7,10 @@ eel.init('web')
 def say_hello_py(x):
     print('Hello from %s' % x)
 
-@eel.expose
-def change():
-    eel.close()
-    eel.start('hello1.html', size=(1280,832))
+
 
 
 say_hello_py('Python World!')
 eel.say_hello_js('Python World!')   # Call a Javascript function
 
-eel.start('hello.html', size=(1280, 832))  # Start
+eel.start('page3.html', size=(1280, 832))  # Start
