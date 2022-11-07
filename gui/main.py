f7d5d59ffb502e60fe46deee5e1a2ff957a36726
delibae <82585834+delibae@users.noreply.github.com>
2022-11-08 02:46:10 +0900

    page1 to page3 data trans complete

--- gui/main.py
+++ gui/main.py
@@ -1,4 +1,7 @@
 import eel
+import pandas as pd
+import json
+from pandas import json_normalize
 
 # Set web files folder
 eel.init('web')
@@ -7,10 +10,15 @@ eel.init('web')
 def say_hello_py(x):
     print('Hello from %s' % x)
 
-
-
+@eel.expose
+def get_file(file):
+    print(type(file))
+    info = json.loads(file)
+    df = json_normalize(info)
+    ad_list = df['도로명']
+    print(ad_list)
 
 say_hello_py('Python World!')
 eel.say_hello_js('Python World!')   # Call a Javascript function
 
-eel.start('page5.html', size=(1280, 832))  # Start
+eel.start('/page1.html', size=(1280, 832))  # Start
