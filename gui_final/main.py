6b4391ff79945d9458cea7a9ce4675a0f8d6126f
delibae <82585834+delibae@users.noreply.github.com>
2022-11-14 20:28:07 +0900

    finishgit add .git add .git add .git add .git add .

---
+++ gui_final/main.py
@@ -0,0 +1,220 @@
+import eel
+
+import json
+from pandas import json_normalize
+import route_module as rt
+import time
+import pickle
+
+time_gap,time1 = rt.get_time_gap()
+
+
+start_time = 0
+
+
+
+with open('./data/recent1.pickle', 'rb') as f:
+    recent1 = pickle.load(f)
+print(recent1)
+with open('./data/recent2.pickle', 'rb') as f:
+    recent2 = pickle.load(f)
+print(recent2)
+with open('./data/recent3.pickle', 'rb') as f:
+    recent3 = pickle.load(f)
+print(recent3)
+with open('./data/time_dict.pickle', 'rb') as f:
+    time_dict = pickle.load(f)
+print(time_dict)
+with open('./data/address_dict.pickle', 'rb') as f:
+    address_dict = pickle.load(f)
+print(address_dict)
+with open('./data/convert_address_dict.pickle', 'rb') as f:
+    convert_address_dict = pickle.load(f)
+print(convert_address_dict)
+
+# Set web files folder
+eel.init('web')
+
+is_new = 0
+
+@eel.expose  # Expose this function to Javascript
+def say_hello_py(x):
+    print('Hello from %s' % x)
+
+
+@eel.expose
+def get_file(file):
+
+    print(type(file))
+    info = json.loads(file)
+    df = json_normalize(info)
+    global ad_list
+    ad_list = df['도로명']
+    global addressList
+    addressList = ad_list.to_list()
+    global is_new
+    is_new = 1
+
+
+    print(ad_list)
+
+    ####ad1_list ad2_list ad3_list pickle 바꾸기
+
+
+### ad_list => address_list 전환 필요
+@eel.expose
+def find_path():
+    if is_new == 1:
+        ## 최신 파일 교체
+        with open('./data/recent2.pickle', 'wb') as f:
+            pickle.dump(recent1, f, pickle.HIGHEST_PROTOCOL)
+        with open('./data/recent3.pickle', 'wb') as f:
+            pickle.dump(recent2, f, pickle.HIGHEST_PROTOCOL)
+        with open('./data/recent1.pickle', 'wb') as f:
+            pickle.dump(addressList, f, pickle.HIGHEST_PROTOCOL)
+
+
+    routeGraph = rt.Graph(len(addressList), addressList, address_dict, time_dict)
+    pivot = address_dict.get(addressList[0])
+    ### n 값 결정 알고리즘 필요
+
+    target_second = 2
+    n = rt.find_n(len(addressList),target_second,time_gap,time1)
+    print("len of addressList", len(addressList))
+    print("calculated n: ",n)
+    a = rt.frm(routeGraph, n, pivot,routeGraph.serialNumList)
+
+    global path_time
+    pivot, path, path_time = a.complete_path()
+
+    global final_ad
+    final_ad = rt.serial_to_ad(path,convert_address_dict)
+    print(final_ad)
+
+    eel.check()
+
+    ### 나중에 지울것
+    time.sleep(1)
+    global start_time
+
+    start_time = time.time()
+
+    eel.next_page()
+
+
+
+i = 1
+
+
+
+@eel.expose
+def on_load():
+    global path_size
+    global i
+    global final_ad
+    global path_time
+    global start_time
+
+
+    print("i", i)
+    path_size = len(final_ad)
+
+    current_ad = final_ad[i]
+    rest_time = sum(path_time[:])
+    ctime= round(time.time() - start_time)
+
+    eel.first_data(current_ad, str(rest_time), str(ctime//60))
+
+@eel.expose
+def b_load():
+    global path_size
+    global i
+    global final_ad
+    global path_time
+    global start_time
+
+    print("i", i)
+    path_size = len(final_ad)
+
+    current_ad = final_ad[i]
+    rest_time = path_time[-1]
+    ctime = round(time.time() - start_time)
+    eel.first_data(current_ad, str(rest_time),str(ctime//60))
+@eel.expose
+def next_path():
+    global path_size
+    global i
+    global final_ad
+    global path_time
+    global start_time
+
+    print("i", i)
+    if i < path_size-1:
+        i += 1
+        current_ad = final_ad[i]
+        rest_time = sum(path_time[i-1:])
+        print(rest_time)
+        ctime = round(time.time() - start_time)
+        eel.next_data(current_ad, str(rest_time),str(ctime//60))
+    elif i == path_size -1:
+        eel.last_page()
+
+@eel.expose
+def before_path():
+    global path_size
+    global i
+    global final_ad
+    global path_time
+    global start_time
+
+    print("i", i)
+    if i > 1:
+        i += -1
+
+        current_ad = final_ad[i]
+        rest_time = sum(path_time[i-1:])
+        print(rest_time)
+        ctime = round(time.time() - start_time)
+        eel.before_data(current_ad, str(rest_time),str(ctime//60))
+
+@eel.expose
+def b_check():
+    if i != 1:
+        eel.hide()
+        
+@eel.expose
+def on_page2_load():
+    ###load pickle 필요
+    # global ad1_list
+    # global ad2_list
+    # global ad3_list
+    #
+    # ad1_list = recent1
+    # ad2_list = recent2
+    # ad3_list = recent3
+    print("여기!")
+    ad_pack = [recent1,recent2,recent3]
+    eel.change(ad_pack)
+
+
+@eel.expose
+def to_calculate(num):
+    ###recent pickle로 불러오기
+
+    global addressList
+
+    if num == 3:
+        addressList = recent3
+    elif num == 2:
+        addressList = recent2
+    else:
+        addressList = recent1
+
+    eel.to_page_3()
+
+
+
+say_hello_py('Python World!')
+eel.say_hello_js('Python World!')  # Call a Javascript function
+
+eel.start('/page1.html', size=(1280, 832))  # Start
---
+++ gui_final/route_module.py
@@ -0,0 +1,266 @@
+import pandas as pd
+import time
+import random
+
+addressList = []
+for i in range(100):
+    addressList.append(i)
+addressList2 = []
+for i in range(200):
+    addressList2.append(i)
+
+
+# serialNumList = []
+address_dict = {}
+for i in range(len(addressList2)):
+    address_dict[i] = i
+# # 전체주소-전체주소: 소요시간
+time_dict = {}
+
+for i in range(len(addressList2)):
+    for j in range(i+1,len(addressList2)):
+        time_dict[f'{i}-{j}'] = random.randint(1,100)
+
+class Graph():
+    def __init__(self, size, addressList, address_dict, time_dict):
+        self.SIZE = size
+        self.graph = [[0 for _ in range(size)] for _ in range(size)]
+
+        self.address_dict = address_dict
+        self.time_dict = time_dict
+        self.addressList = addressList
+        self.serialNumList = []
+
+        for i in range(len(self.addressList)):
+            if self.addressList[i] in self.address_dict:
+                self.serialNumList.insert(i, self.address_dict[self.addressList[i]])
+
+        for i in range(len(self.serialNumList)):
+            for j in range(i, len(self.serialNumList)):
+                self.add(i, j)
+
+    def add(self, i, j):
+        if i == j:
+            self.graph[i][j] = 999
+        else:
+            try:
+                time = self.time_dict[str(self.serialNumList[i]) + "-" + str(self.serialNumList[j])]
+            except:
+                time = self.time_dict[str(self.serialNumList[j]) + "-" + str(self.serialNumList[i])]
+            self.graph[j][i] = time
+            self.graph[i][j] = time
+
+    def out(self):
+        for i in self.graph:
+            print(i)
+
+
+# 일련번호 리스트 이용해서 그래프 만들기
+# routeGraph = Graph(len(addressList), addressList, address_dict, time_dict)
+
+
+class frm():
+    def __init__(self,routeGraph, n , pivot, serialNumList):
+        self.routeGraph = routeGraph
+        self.n = n
+        self.pivot = pivot
+
+        self.serialNumList = serialNumList
+
+        self.path = []
+        self.path_time = []
+        self.path.append(self.pivot)
+
+        self.for_visit = [0 for i in range(self.routeGraph.SIZE)]
+        self.for_visit[self.pivot] = 1
+
+        self.Graph_to = pd.DataFrame(self.routeGraph.graph)
+
+        self.seq_n, self.seq_rest = self.seq_in()
+
+    def seq_in(self):
+        if (self.routeGraph.SIZE - 1)%self.n == 0:
+            seq_n = (self.routeGraph.SIZE -1)//self.n
+            seq_rest = None
+        else:
+            seq_n = (self.routeGraph.SIZE -1)//self.n
+            seq_rest = (self.routeGraph.SIZE - 1)%self.n
+        return seq_n,seq_rest
+
+
+    def pp(self,ary, num):
+        result = []
+        if num >= 1:
+            for i in ary:
+                arr = ary.copy()
+                arr.remove(i)
+                get = self.pp(arr,num-1)
+                for j in get:
+                    result.append([i]+j)
+        if num == 1:
+            for i in ary:
+                result.append([i])
+            return result
+        return result
+
+    def find_min(self,num):
+        r = []
+        for i in range(len(self.for_visit)):
+            if self.for_visit[i] == 0:
+                r.append(i)
+        min = 9999
+        min_pth = None
+        min_time = None
+
+        for i in self.pp(r, num):
+            point = self.pivot
+            total = []
+            for j in i:
+                total.append(self.Graph_to.loc[point, j])
+                point = j
+            if sum(total) < min:
+                min = sum(total)
+                min_pth = i
+                min_time = total
+
+        self.path.extend(min_pth)
+
+        self.path_time.extend(min_time)
+        self.pivot = self.path[-1]
+        for i in min_pth:
+            self.for_visit[i] = 1
+
+    def complete_path(self):
+        for i in range(self.seq_n):
+            self.find_min(self.n)
+        if self.seq_rest != None:
+            self.find_min(self.seq_rest)
+        #path to seriul num
+        seriul_path = []
+        for i in self.path:
+            seriul_path.append(self.serialNumList[i])
+        self.path = seriul_path
+        return self.pivot, self.path, self.path_time
+
+#
+# pivot = address_dict.get(addressList[0])
+# n = 2
+# a = frm(routeGraph,n,pivot,routeGraph.serialNumList)
+#
+# pivot, path, path_time = a.complete_path()
+# print(routeGraph.serialNumList)
+# print(path)
+# print(path_time)
+#
+# convert_address_dict = {v:k for k,v in address_dict.items()}
+
+def serial_to_ad(path,convert_address_dict):
+    f_ad = []
+    for i in path:
+        f_ad.append(convert_address_dict.get(i))
+    return f_ad
+
+
+
+# print(convert_address_dict)
+# print(serial_to_ad(path))
+
+# def cl_1000():
+#     start = time.time()
+#     b = []
+#     list = [1]
+#     for i in range(10000):
+#         a = list[0] > list[0]
+#         b.append(a)
+#     close = time.time()
+#     return (close - start)
+#
+# print(cl_1000())
+
+def factorial(n):
+    re = 1
+    for i in range(1, n+1):
+        re *= i
+    return re
+
+def sum_calculate(i,n):
+    total = 0
+    for m in range((i-1)//n):
+        total += factorial(i-1-n*m)/factorial(i-1-n*m-n)
+    if (i-1)%n != 0:
+        total += factorial((i-1)%n)
+    return total
+print(sum_calculate(10,1))
+
+def find_n(i,target,time_gap,time1):
+    standard_time = 0.01
+    t_weight = round(0.01/time_gap)
+    if t_weight == 0:
+        t_weight = 1
+    print("t_weight :" , t_weight)
+    weight = i
+    if i >= 150:
+        max_n = 1*t_weight
+    elif i >= 50:
+        max_n = 2*t_weight
+    elif i >= 20:
+        max_n = 3*t_weight
+    elif i >= 10:
+        max_n = 4
+    else:
+        max_n = 999
+    print("weight: ",weight)
+    load_time = weight*time1
+    print("loda_time: ",load_time)
+
+    if time_gap > 0:
+        num1 = sum_calculate(100,10)
+        num2 = sum_calculate(200,10)
+        gap = num2 - num1
+        unit_time = (time_gap/gap)*weight
+        target = target - load_time
+        print("target: ",target)
+        max_cal = target//unit_time + num1
+        print("max_cal: ", max_cal)
+        for j in range(1,i):
+            if sum_calculate(i,j) > max_cal:
+                if j > max_n:
+                    return max_n
+                return j
+
+        if i-1 > max_n:
+            return max_n
+        return i-1
+
+    else:
+        if i-1 > max_n:
+            return max_n
+        return i-1
+
+
+
+
+def get_time_gap():
+    routeGraph1 = Graph(len(addressList), addressList, address_dict, time_dict)
+    routeGraph2 = Graph(len(addressList2), addressList2, address_dict, time_dict)
+    pivot = address_dict.get(addressList[0])
+    ### n 값 결정 알고리즘 필요
+    n = 1
+    a = frm(routeGraph1, n, pivot, routeGraph1.serialNumList)
+    n = 10
+    start = time.time()
+    a = frm(routeGraph1, n, pivot, routeGraph1.serialNumList)
+    time1 = time.time() - start
+    print("time1: ", time1)
+    n = 10
+    start = time.time()
+    a = frm(routeGraph2, n, pivot, routeGraph2.serialNumList)
+    time2 = time.time() - start
+    print("time2: ", time2)
+    time_gap = time2 - time1
+    print("time_gap: ", time_gap)
+
+    return time_gap, time1
+
+time_gap,time1 = get_time_gap()
+print(find_n(100,2,time_gap,time1))
